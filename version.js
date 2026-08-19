967cb3a7449c1130c6f90e8e49350d712bc6401c
wellmattos <wellington.mattos@gmail.com>
2026-08-18 23:05:13 -0300

    Update version.js

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
-version: 32,                                    // número inteiro, MAIOR que a versão anterior
+version: 33,                                    // número inteiro, MAIOR que a versão anterior
 url: "https://ibm.box.com/s/khf0lgzv52iy4blpm0gmxaphvpkfnudn",   // link do OneDrive/SharePoint
 notas: "Novidades da Versão 32",    // texto do aviso (opcional)
